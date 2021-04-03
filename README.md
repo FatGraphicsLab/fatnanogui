6d1e81646671dccf706232ef60fc1fa8f135fc29
kasicass <kasicass@gmail.com>
2021-04-03 14:45:50 +0800

    init

---
+++ README.md
@@ -0,0 +1,7 @@
+# fatnanogui
+
+* a clone of [nanogui][1]
+* just for fun~ 
+
+
+[1]:https://github.com/wjakob/nanogui
---
+++ samples/example1.cpp
@@ -0,0 +1,10 @@
+#include <nanogui/common.h>
+
+int main(int argc, char* argv[])
+{
+    nanogui::init();
+
+    nanogui::shutdown();
+
+    return 0;
+}
